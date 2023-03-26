6999f981caa6fb37d3de89f842dd9a35bdff89ef
GPlastok <85394445+GPlastok@users.noreply.github.com>
2023-03-26 21:22:17 +0200

    first part of js for sidebar toggle and dark mode

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 "use strict";
 
 //abstract function
-function AppearElementFunc(targetElementClass, className, delay = 0) {
+function appearElementFunc(targetElementClass, className, delay = 0) {
   // Logic to make an element appear on DOM
   const targetElement = document.querySelector(targetElementClass);
   //If statement to avoid errors if the target element is not found.
-  //We could use if (tergetElement), but the version below is more correct.
+  //We could use if (targetElement), but the version below is more correct.
   if (targetElement !== null) {
     setTimeout(() => {
       targetElement.classList.remove(className);
@@ -13,13 +13,37 @@ function AppearElementFunc(targetElementClass, className, delay = 0) {
   }
 }
 
+//---------------MENU TOGGLE EVENT-------------------
+
+function toggleMenu(buttonClassName) {
+  const toggleMenuElement = document.querySelector(buttonClassName);
+
+  console.log(toggleMenuElement);
+
+  if (toggleMenuElement !== null) {
+    toggleMenuElement.addEventListener("click", onMenuToggleClick);
+  }
+}
+
+function onMenuToggleClick(event) {
+  console.log(`hello from ${event.target}`);
+  event.target.innerHTML = "clicked";
+}
+
+//---------------------------------------------------
+//----------START PROGRAM-----------------------------
+//
 //If the dom is loaded we make two messages appear with different timing
 window.addEventListener("DOMContentLoaded", () => {
   //funtion instances
-  new AppearElementFunc(".test-message", "display-none", 0);
-  new AppearElementFunc(".test-message-two", "display-none", 0);
+  appearElementFunc(".test-message", "display-none", 1500);
+  appearElementFunc(".test-message-two", "display-none", 2000);
+
+  toggleMenu(".menu-toggle");
 
   //A previous example showing possibilities. kept just for personal reference:
+  //This works only when we have this.examplelement
+  //new functionblahblah only when function uses this.fdetffyu
   //const secondElement = new AppearElement(".test-message-two", "display-none", 10000);
   // secondElement.targetElement.classList.add("alie");
   //  test
