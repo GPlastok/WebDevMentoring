ceeec7013cc86c9ad6a9d99e9cf4126c8a154a5d
Georgios <georgios.plastok@gmail.com>
2023-03-27 21:34:49 +0200

    completed basic sidebar on off .  fixerr parameter

--- src/index.js
+++ src/index.js
@@ -15,19 +15,63 @@ function appearElementFunc(targetElementClass, className, delay = 0) {
 
 //---------------MENU TOGGLE EVENT-------------------
 
-function toggleMenu(buttonClassName) {
+// function toggleMenu(buttonClassName) {
+//   const toggleMenuElement = document.querySelector(buttonClassName);
+
+//   console.log(toggleMenuElement);
+
+//   if (toggleMenuElement !== null) {
+//     toggleMenuElement.addEventListener("click", onMenuToggleClick);
+//   }
+// }
+
+// function onMenuToggleClick(event) {
+//   console.log(`hello from ${event.target}`);
+//   event.target.innerHTML = "clicked";
+// }
+
+//The parameter toggleBool is good for temporarily switching through code, but it's
+//probably not needed for the final version
+
+function toggleMenu(
+  buttonClassName,
+  // toggleBool,
+  affectedElementClassName,
+  switchClassNameString
+) {
   const toggleMenuElement = document.querySelector(buttonClassName);
+  const affectedElement = document.querySelector(affectedElementClassName);
 
   console.log(toggleMenuElement);
+  console.log(affectedElement);
+  //test to remove
+  // if (affectedElement.classList.contains(switchClassNameString)) {
+  //   console.log(`switch class is in`);
+  // }
+  //FIX =========>>>>>   switchClassNameString doesnt work. error
+  if (toggleMenuElement !== null && affectedElement !== null) {
+    toggleMenuElement.addEventListener("click", () => {
+      console.log(`affectedElement switch is clicked`);
+      console.log(affectedElement.classList);
 
-  if (toggleMenuElement !== null) {
-    toggleMenuElement.addEventListener("click", onMenuToggleClick);
+      if (affectedElement.classList.contains("sidebar--closed")) {
+        console.log(`affected element contains class`);
+        affectedElement.classList.remove("sidebar--closed");
+
+        console.log(`affected element classlist remove`);
+        // toggleBool = false;
+        // console.log(switchClassNameString);
+      } else if (
+        affectedElement.classList.contains("sidebar--closed") === false
+      ) {
+        console.log(`class not found`);
+        affectedElement.classList.add("sidebar--closed");
+        // toggleBool = true;
+        // toggleBool.target.innerHTML = "clicked";
+      }
+      // console.log(`hello from ${toggleBool}`);
+    });
   }
-}
-
-function onMenuToggleClick(event) {
-  console.log(`hello from ${event.target}`);
-  event.target.innerHTML = "clicked";
 }
 
 //---------------------------------------------------
@@ -39,7 +83,9 @@ window.addEventListener("DOMContentLoaded", () => {
   appearElementFunc(".test-message", "display-none", 1500);
   appearElementFunc(".test-message-two", "display-none", 2000);
 
-  toggleMenu(".menu-toggle");
+  //MOVE THE BOOL BELOW IN PROGRAM START (This is to be used as toggleBool in the toggleMenu function)
+  // let menuOn = true;
+  toggleMenu(".menu-toggle", ".sidebar", "sidebar--closed");
 
   //A previous example showing possibilities. kept just for personal reference:
   //This works only when we have this.examplelement
