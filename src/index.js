1d82b1c63e60fab90847205bbe6d7356bc46ad39
inhaltone <inhalt.xyz@gmail.com>
2023-04-27 21:20:30 +0300

    fix: add flex container at the first card + handle modal listener

--- src/index.js
+++ src/index.js
@@ -2,87 +2,99 @@
 
 //abstract function
 function appearElementFunc(targetElementClass, className, delay = 0) {
-  // Logic to make an element appear on DOM
-  const targetElement = document.querySelector(targetElementClass);
-  //If statement to avoid errors if the target element is not found.
-  //We could use if (targetElement), but the version below is more correct.
-  if (targetElement !== null) {
-    setTimeout(() => {
-      targetElement.classList.remove(className);
-    }, delay);
-  }
+    // Logic to make an element appear on DOM
+    const targetElement = document.querySelector(targetElementClass);
+    //If statement to avoid errors if the target element is not found.
+    //We could use if (targetElement), but the version below is more correct.
+    if (targetElement !== null) {
+        setTimeout(() => {
+            targetElement.classList.remove(className);
+        }, delay);
+    }
 }
 
 //---------------MENU TOGGLE EVENT-------------------
 
 function toggleMenu(
-  buttonClassName,
-  affectedElementClassName,
-  switchClassNameString,
-  buttonLabel = {}
+    buttonClassName,
+    affectedElementClassName,
+    switchClassNameString,
+    buttonLabel = {}
 ) {
-  // Declaration of local variables and local statements as functions
-  let isMenuExpanded = false;
-  const toggleMenuElement = document.querySelector(buttonClassName); // button switch DOM
-  const affectedElement = document.querySelector(affectedElementClassName); // target element DOM
+    // Declaration of local variables and local statements as functions
+    let isMenuExpanded = false;
+    const toggleMenuElement = document.querySelector(buttonClassName); // button switch DOM
+    const affectedElement = document.querySelector(affectedElementClassName); // target element DOM
 
-  function updateButtonLabel() {
-    toggleMenuElement.innerText = isMenuExpanded
-      ? buttonLabel.close
-      : buttonLabel.open;
-  }
+    function updateButtonLabel() {
+        toggleMenuElement.innerText = isMenuExpanded
+            ? buttonLabel.close
+            : buttonLabel.open;
+    }
 
-  function fireToggle() {
-    // on each click this function runs
-    isMenuExpanded = !isMenuExpanded;
-    affectedElement.classList.toggle(switchClassNameString);
-    updateButtonLabel();
-  }
+    function fireToggle() {
+        // on each click this function runs
+        isMenuExpanded = !isMenuExpanded;
+        affectedElement.classList.toggle(switchClassNameString);
+        updateButtonLabel();
+    }
 
-  if (toggleMenuElement !== null && affectedElement !== null) {
-    // check if elements are rendered to DOM
-    updateButtonLabel(); // 1st call of function
-    toggleMenuElement.addEventListener("click", fireToggle); // 2nd call of function
-  }
+    if (toggleMenuElement !== null && affectedElement !== null) {
+        // check if elements are rendered to DOM
+        updateButtonLabel(); // 1st call of function
+        toggleMenuElement.addEventListener("click", fireToggle); // 2nd call of function
+    }
 }
 
 // --------MODAL TOGGLE EVENT-------------
 
 function toggleModal(
-  buttonClassName,
-  affectedElementClassName,
-  switchClassNameString
+    buttonClassName,
+    affectedElementClassName,
+    switchClassNameString
 ) {
-  // let isModalOpen = false;
-  const toggleModalElements = document.querySelectorAll(buttonClassName); // button switch DOM
-  const affectedElement = document.querySelector(affectedElementClassName);
+    // let isModalOpen = false;
+    const toggleModalElements = document.querySelectorAll(buttonClassName); // button switch DOM
+    const affectedElement = document.querySelector(affectedElementClassName);
 
-  // console.log(affectedElement, switchClassNameString);
-  // console.log(affectedElement.classList);
-  function fireToggle() {
-    console.log("fireToggle called"); // Debugging line
+    // console.log(affectedElement, switchClassNameString);
+    // console.log(affectedElement.classList);
 
-    // on each click this function runs
+    function isModalOpen() {  // return true/false (getter function)
+        return !affectedElement.classList.contains(switchClassNameString);
+    }
 
-    // isModalOpen = !isModalOpen;
+    function fireToggle() {
+        console.log("fireToggle called"); // Debugging line
 
-    affectedElement.classList.toggle(switchClassNameString);
+        // on each click this function runs
 
-    console.log(`fireToggle works all the time`);
-  }
+        // isModalOpen = !isModalOpen;
 
-  if (toggleModalElements !== null && affectedElement !== null) {
-    console.log("Adding event listeners"); // Debugging line
-    // check if elements are rendered to DOM
-    toggleModalElements.forEach((element) => {
-      element.addEventListener("click", fireToggle);
-    }); // 1st call of function
+        affectedElement.classList.toggle(switchClassNameString);
 
-    //--Here, ONLY if modal is open close it
-    //   if (affectedElement.classList) {
-    //   toggleModalElement.addEventListener("keydown", fireToggle); //2nd call of function
-    // }
-  }
+        console.log(`fireToggle works all the time`);
+    }
+
+    function handleModalClick(event) { // callback function from key listener
+        if (event.key === 'Escape' && isModalOpen()) {
+            fireToggle(); // close the modal
+        }
+    }
+
+    if (toggleModalElements !== null && affectedElement !== null) {
+        console.log("Adding event listeners"); // Debugging line
+        // check if elements are rendered to DOM
+        toggleModalElements.forEach((element) => {
+            element.addEventListener("click", fireToggle);
+            element.addEventListener("keydown", handleModalClick);
+        }); // 1st call of function
+
+        //--Here, ONLY if modal is open close it
+        //   if (affectedElement.classList) {
+        //   toggleModalElement.addEventListener("keydown", fireToggle); //2nd call of function
+        // }
+    }
 }
 
 //----------------------------------------------------
@@ -90,25 +102,25 @@ function toggleModal(
 //
 //If the dom is loaded we make two messages appear with different timing
 window.addEventListener("DOMContentLoaded", () => {
-  //funtion instances
-  appearElementFunc(".test-message", "display-none", 1500);
-  appearElementFunc(".test-message-two", "display-none", 2000);
+    //funtion instances
+    appearElementFunc(".test-message", "display-none", 1500);
+    appearElementFunc(".test-message-two", "display-none", 2000);
 
-  toggleMenu(".menu-toggle", ".sidebar", "sidebar--closed", {
-    close: "CLOSE",
-    open: "MENU",
-  });
+    toggleMenu(".menu-toggle", ".sidebar", "sidebar--closed", {
+        close: "CLOSE",
+        open: "MENU",
+    });
 
-  toggleModal(".btn-toggle-modal", ".modal-container", "modal--closed");
+    toggleModal(".btn-toggle-modal", ".modal-container", "modal--closed");
 
-  //A previous example showing possibilities. kept just for personal reference:
-  //This works only when we have this.examplelement
-  //new functionblahblah only when function uses this.whatever
-  //const secondElement = new AppearElement(".test-message-two", "display-none", 10000);
-  // secondElement.targetElement.classList.add("alie");
-  //  test
+    //A previous example showing possibilities. kept just for personal reference:
+    //This works only when we have this.examplelement
+    //new functionblahblah only when function uses this.whatever
+    //const secondElement = new AppearElement(".test-message-two", "display-none", 10000);
+    // secondElement.targetElement.classList.add("alie");
+    //  test
 
-  //test to delete:
-  // const buttonTest = document.querySelector(`.btn-toggle-modal`);
-  // console.log.all(`buttonTest: ${buttonTest}`);
+    //test to delete:
+    // const buttonTest = document.querySelector(`.btn-toggle-modal`);
+    // console.log.all(`buttonTest: ${buttonTest}`);
 });
